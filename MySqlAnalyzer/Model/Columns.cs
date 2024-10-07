4751d352735e9b938dffd8abf13586bcd5a933f1
Herbie68 <herbert.nijkamp@gmail.com>
2024-10-07 16:15:36 +0200

    Created Columns Model

---
+++ MySqlAnalyzer/Model/Columns.cs
@@ -0,0 +1,22 @@
+﻿namespace MySqlAnalyzer.Model;
+public class Columns
+{
+	public int? ColumnTableId { get; set; }
+	public string? ColumnTableName { get; set; }
+	public string? ColumnName { get; set; }
+	public string? ColumnType { get; set; }
+	public string? ColumnColumnKey { get; set; }
+	public string? ColumnPosition { get; set; }
+	public string? ColumnDefault { get; set; }
+	public string? ColumnNullable { get; set; }
+	public string? ColumnDataType { get; set; }
+	public string? ColumnDataLength { get; set; }
+	public string? ColumnNumericPrecision { get; set; }
+	public string? ColumnNumericScale { get; set; }
+	public string? ColumnDateTimePrecision { get; set; }
+	public string? ColumnCharacterSet { get; set; }
+	public string? ColumnCollation { get; set; }
+	public string? ColumnExtra { get; set; }
+	public string? ColumnPrivileges { get; set; }
+	public string? ColumnComment { get; set; }
+}
