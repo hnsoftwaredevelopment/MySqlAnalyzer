b4dea12c02e75858871fbbace382b5a04fc2ad0c
Herbie68 <herbert.nijkamp@gmail.com>
2024-10-10 17:04:09 +0200

    Strugling with the FilterRow Check https://help.syncfusion.com/wpf/datagrid/filterrow

---
+++ MySqlAnalyzer/Converters/AllowFilteringToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace MySqlAnalyzer.Converters;
+public class AllowFilteringToVisibilityConverter : IValueConverter
+{
+	public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+	{
+		if ( value is bool allowFiltering )
+		{
+			return allowFiltering ? Visibility.Visible : Visibility.Collapsed;
+		}
+		return Visibility.Collapsed;
+	}
+
+	public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+	{
+		throw new NotImplementedException();
+	}
+}
--- MySqlAnalyzer/MainWindow.xaml.cs
+++ MySqlAnalyzer/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows;
-
-namespace MySqlAnalyzer;
+﻿namespace MySqlAnalyzer;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -10,12 +8,8 @@ public partial class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
-		List<TableModel> TableList = DBCommands.GetStructure(DBNames.Database, DBNames.TableTypeTable);
-		List<TableModel> ViewList = DBCommands.GetStructure(DBNames.Database, DBNames.TableTypeView);
-		List<RoutineModel> FunctionList = DBCommands.GetRoutines(DBNames.Database, DBNames.RoutineTypeFunction);
-		List<RoutineModel> ProcedureList = DBCommands.GetRoutines(DBNames.Database, DBNames.RoutineTypeProcedure);
-		List<ColumnModel> TableDefinitionList = DBCommands.GetTableStructure(DBNames.Database, TableList);
-		List<ColumnModel> ViewDefinitionList = DBCommands.GetTableStructure(DBNames.Database, ViewList);
-		//Console.WriteLine( TableList.Count.ToString(), ViewList.Count.ToString(), FunctionList.Count.ToString(), ProcedureList.Count.ToString() );
+
+		// Check: https://help.syncfusion.com/wpf/datagrid/filterrow
 	}
+
 }
