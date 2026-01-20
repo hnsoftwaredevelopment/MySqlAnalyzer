3570b3a39d8074b893ccd3c56246d14e331887d2
Herbert Nijkamp <herbie68@users.noreply.github.com>
2026-01-20 14:12:57 +0100

    Update application.
    Revamp icons and key indicators in UI
    
    - Replaced old icons with new vector-based, color-coded icons for MySQL, HN Software, and all major database object types (PK, FK, unique, SQL, column, index, procedure)
    - Updated Columns DataGrid to show key type icons instead of text, using a new StringToVisibilityConverter
    - Improved XAML organization with region comments
    - Updated assembly version to 26.1.81.0
    - Enhances clarity and modernizes the application's visual design

---
+++ MySqlAnalyzer/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlAnalyzer.Converters;
+
+public class StringToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var stringValue = value as string;
+        var targetValue = parameter as string;
+        return stringValue == targetValue ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
